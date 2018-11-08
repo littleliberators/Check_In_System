bbf7923fe0e5089a31068c92fee2b0a28a760b8e
Emma Tsipan <s708959@sbuniv.edu>
2018-11-08 05:20:38 +0000

    Made keypad functional

--- Keypad.js
+++ Keypad.js
@@ -1,31 +1,34 @@
 /* global $*/
 
-function load() {
-    var array = new Array();
- 
-    while (array.length < 10) {
-        var temp = Math.round(Math.random() * 9);
-        if (!contain(array, temp)) {
-            array.push(temp);
-        }
+$(function() {
+    let buttons = $("#keypad button");
+    buttons.each(function(index) {
+        $(this).click(input);
+    })
+});
+
+function input() {
+    // Current pin value
+    let pinVal = document.getElementById("PIN-textbox").value;
+
+    // The back button was clicked
+    if (this.innerText == "") {
+        pinVal = pinVal.slice(0, -1);
     }
-    for (var i = 0; i < 10; i++) {
-        var keyButton = document.getElementById("keyButton" + i); //Assuming the ID of the Keys
-        keyButton.value = array[i];
+    // The OK button was clicked
+    else if (this.innerText == "OK") {
+        // This is where the pin needs to be validated and user taken to the next screen
     }
+    // Any other number on the keypad was clicked
+    else {
+        let newVal = this.innerText;
+        pinVal += newVal;
+    }
+
+    $("#PIN-textbox").val(pinVal);
 }
 
-function Input(e) 
-{
-    var keyInput = document.getElementById("keyInput");
-    keyInput.value = keyInput.value + e.value;
+function forgotPIN() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
 }
- 
-function Delete() 
-{
-    var keyInput = document.getElementById("keyInput");
-    keyInput.value = keyInput.value.substr(0, keyInput.value.length - 1);
-}
-
-
-
