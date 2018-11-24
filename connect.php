73d3ee9e7bc1ee2b63084bb324ee2f8f9d8508f4
Austin Newman <s708086@sbuniv.edu>
2018-11-24 01:14:37 +0000

    Connects!

--- connect.php
+++ connect.php
@@ -1,3 +1,21 @@
 <?php
-    $mysqli = mysqli_connect('127.0.0.1','user','','dummy-data.sql') or die(mysqli_error($mysqli));
-?> 
+
+//This is to check the connection 
+
+$host = getenv("https://ide.c9.io/austinn14/senior_project");
+$username = "austinn14";
+$password = "";
+$database = "dummy-data.sql";
+$dbport = 3306;
+
+// Create connection
+$db = mysqli_connect($host, $username, $password, $database, $dbport);
+
+// Check connection
+if ($db->connect_error) 
+{
+    die("Connection failed: " . $db->connect_error);
+} 
+
+echo "Connected Successfully(".$db->host_info.")";
+?>
