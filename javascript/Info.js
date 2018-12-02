12e6900a6d08dcebbf6c98097ba7148afed638bf
Emma Tsipan <s708959@sbuniv.edu>
2018-12-02 05:07:37 +0000

    Added validation when signing in/out

--- javascript/Info.js
+++ javascript/Info.js
@@ -1,29 +1,59 @@
 /* global $*/
 
 $(function() {
+    /*
+        // If the sign in/out buttons get clicked
+        $('.sign-btn').on('click', function() {
+            // Show the log form
+            $('.log-time-popup').show();
+            $('.overlay').show();
+            
+            // Change form titles
+            $('#header').text($(this).text());
+            
+            // Populate time and date fields with current values
+            populateDateTime();
+            
+            // Put focus in signature box
+            $('#e-sign-input').focus()
+        })*/
 
-    // If the sign in/out buttons get clicked
-    $('.sign-btn').on('click', function() {
-        // Show the log form
-        $('.log-time-popup').show();
-        $('.overlay').show();
-        
-        // Change form titles
-        $('#header').text($(this).text());
-        
-        // Populate time and date fields with current values
-        populateDateTime();
-        
-        // Put focus in signature box
-        $('#e-sign-input').focus()
-    })
-    
     // Close the form after close button is clicked 
     $('#close-button').on('click', function() {
         $('.log-time-popup').hide();
         $('.overlay').hide();
     })
 });
+
+function signInForm() {
+    // Show the log form
+    $('.log-time-popup').show();
+    $('.overlay').show();
+
+    // Change form titles
+    $('#header').text("Sign In");
+
+    // Populate time and date fields with current values
+    populateDateTime();
+
+    // Put focus in signature box
+    $('#e-sign-input').focus();
+}
+
+function signOutForm() {
+    // Show the log form
+    $('.log-time-popup').show();
+    $('.overlay').show();
+
+    // Change form titles
+    $('#header').text("Sign Out");
+
+    // Populate time and date fields with current values
+    populateDateTime();
+
+    // Put focus in signature box
+    $('#e-sign-input').focus();
+}
 
 function populateDateTime() {
     var d = new Date(),
@@ -41,4 +71,9 @@ function populateDateTime() {
     var today = year + "-" + month + "-" + day;
     $('#time-input').val(now);
     $('#date-input').val(today);
+}
+
+// Show error message 'Please select one child'
+function showSelectChild(){
+    $('#please-select').show();
 }
--- php/Child-Info.php
+++ php/Child-Info.php
@@ -26,108 +26,61 @@ session_start();
       <img id="rightimg" src="../images/Right_Toys.png" align="Right" alt="Right Toys">
     </div>
     <div class="row-child" id="child-container">
-        <div>
-            
-            <!-- script to handle checkboxes -->
-            <script type="text/javascript">
-            /* global $*/
-            var allchecked;
-                $(function(){
-                    // Check all boxes if "Select All" is clicked
-                    $('#select-all').click(function(event) {   
-                        if(this.checked) {
-                          $(':checkbox').each(function() {
-                            this.checked = true;                        
-                          });
-                        }
-                        else {
-                          $(':checkbox').each(function() {
-                            this.checked = false;
-                          });
-                        }
-                    });
-                      
-                    // Uncheck "Select All" if any other checkbox is unchecked  
-                    $(':checkbox').click(function(event) {
-                        $(':checkbox').each(function() {
-                        if (this.checked == false){
-                           $('#select-all').prop('checked', false);
-                        }
-                    });
-                    
-                    
-                    // Check "Select All" if all other checkboxes are checked
-                    $(':checkbox').change(function() {
-                        allchecked = true;
-                        $('.check').each(function() {
-                            if (this.checked == false) {
-                                allchecked = false;
-                            }
-                        });
-                        if (allchecked == true) {
-                            $('#select-all').prop('checked', true);
-                        }
-                        allchecked = "";
-                    });
-                })
-            })
-            </script>
-            
-            <form class="select-student" action="processlist.php" method="post">
-                <div class = "check-all-row">
-                    <input type="checkbox" name="select-all" id="select-all" value="Select All"/>
-                    <label class="label" id="select-all-label" for="select-all">Select All</label>
-                </div>
+        <form class="select-student" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <div class = "check-all-row">
+                <input type="checkbox" name="select-all" id="select-all" value="Select All"/>
+                <label class="label" id="select-all-label" for="select-all">Select All</label>
+            </div>
                 
-                <!-- Pull student data from sql -->
-                <?php
-                    $FamID = $_SESSION["FamilyID"];
-                    
-                    // Database credentials
-                    $host = "127.0.0.1";
-                    $user = "emmatsipan";
-                    $pass = "";
-                    $db = "little_liberators";
-                    $port = 3306;
-                    
-                    // Connect to the database
-                    $dbc = mysqli_connect($host, $user, $pass, $db, $port);
-                    
-                    // Check connection
-                    if ($dbc->connect_error) {
-                       die("Connection failed: " . $cdbc->connect_error);
-                    } 
-                    
-                    $query = "SELECT First_Name, Last_Name
-                           FROM Child
-                           WHERE Family_ID = '$FamID'";
-                    $result = mysqli_query($dbc, $query);
-                    
-                    $num_rows = $result->num_rows;
-                    
-                    //Iterate over the results that we got from the database
-                    if ($num_rows > 0){
-                        while($row = mysqli_fetch_assoc($result)) {
-                        ?>
-                            <!-- Create a label and checkbox for each child -->
-                            <div class="checkbox-row">
-                                <input class="check" type="checkbox" name="Name[]" id='<?php echo $row["First_Name"] . "-" . $row["Last_Name"]; ?>'
-                                value='<?php echo $row["First_Name"] . " " . $row["Last_Name"]; ?>'/>
-                                <label class="label" for='<?php echo $row["First_Name"] . "-" . $row["Last_Name"]; ?>'>
-                                    <?php echo $row["First_Name"] . " " . $row["Last_Name"]; ?>
-                                </label><br/>
-                            </div>
-                        <?php
-                       }
-                    }
-                ?>
-            </form>
-        </div> 
-    </div>
-    <div class="row-child" id="child-info-btn">
-        <button class="sign-btn" id="sign-in-btn">Sign In</button>
-        <button class="sign-btn" id="sign-out-btn">Sign Out</button>
-    </div>
+            <!-- Pull student data from sql -->
+            <?php
+                $FamID = $_SESSION["FamilyID"];
+                
+                // Database credentials
+                $host = "127.0.0.1";
+                $user = "emmatsipan";
+                $pass = "";
+                $db = "little_liberators";
+                $port = 3306;
+                
+                // Connect to the database
+                $dbc = mysqli_connect($host, $user, $pass, $db, $port);
+                
+                // Check connection
+                if ($dbc->connect_error) {
+                   die("Connection failed: " . $cdbc->connect_error);
+                } 
+                
+                $query = "SELECT Child_ID, First_Name, Last_Name
+                       FROM Child
+                       WHERE Family_ID = '$FamID'";
+                $result = mysqli_query($dbc, $query);
+                
+                $num_rows = $result->num_rows;
+                
+                //Iterate over the results that we got from the database
+                if ($num_rows > 0){
+                    while($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                        <!-- Create a label and checkbox for each child -->
+                        <div class="checkbox-row">
+                            <input class="check" type="checkbox" name="Name[]" id='<?php echo $row["First_Name"] . "-" . $row["Last_Name"]; ?>'
+                            value='<?php echo $row["Child_ID"]; ?>'/>
+                            <label class="label" for='<?php echo $row["First_Name"] . "-" . $row["Last_Name"]; ?>'>
+                                <?php echo $row["First_Name"] . " " . $row["Last_Name"]; ?>
+                            </label><br/>
+                        </div>
+                    <?php
+                   }
+                }
+            ?>
+            </div> 
+            <div class="hide" id="please-select">Please select at least one child.</div>
+            <div class="row-child" id="child-info-btn">
+                <button class="sign-btn" id="sign-in-btn" name="signinbutton">Sign In</button>
+                <button class="sign-btn" id="sign-out-btn" name="signoutbutton">Sign Out</button>
+            </div>
+        </form>
     <div class="overlay hideform"></div>
     <!-- Time log popup -->
     <div class="log-time-popup hideform">
@@ -135,13 +88,13 @@ session_start();
             <div id="header">Log Time</div>
             <button id="close-button" aria-label="Close" >X</button>
         </div>
-        <form id="log-time">
+        <form id="log-time" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
             <div class="row" id="date-time-container">
                 <div class="text-label" id="day-label">Day:</div>
-                <input class="input-box" id="date-input" type="date" required 
+                <input class="input-box" id="date-input" name="date" type="date" required disabled
                 oninvalid="this.setCustomValidity('Valid date required')" oninput="this.setCustomValidity('')"/>
                 <div class="text-label" id="time-label">Time:</div>
-                <input class="input-box" id="time-input" type="time" required 
+                <input class="input-box" id="time-input" name="time" type="time" required disabled
                 oninvalid="this.setCustomValidity('Valid time required')" oninput="this.setCustomValidity('')"/>
             </div>
             <div class="row">
@@ -152,15 +105,157 @@ session_start();
             <div class="row">
                 <div id="e-sign-container">
                     <div id="x">X</div>
-                    <input id="e-sign-input" type="text" autocomplete="off" tab-index="0" required pattern="[^\d]*"
+                    <input id="e-sign-input" name="e-sign" type="text" autocomplete="off" tab-index="0" required pattern="[^\d]*"
                     oninvalid="this.setCustomValidity('Please provide an electronic signature')" oninput="this.setCustomValidity('')">
                 </div>
             </div>
             <div>
-                <button id="submit-log" type="submit">Submit</button>
+                <button id="submit-log" type="submit" name="submit">Submit</button>
             </div>
         </form>
     </div>
+    
+    <!-- script to handle checkbox changes -->
+    <script type="text/javascript">
+        /* global $*/
+        var allchecked;
+        $(function(){
+            
+            // Check all boxes if "Select All" is clicked
+            $('#select-all').click(function(event) {   
+                if(this.checked) {
+                  $(':checkbox').each(function() {
+                    $(this).prop('checked', true);                       
+                  });
+                }
+                else {
+                  $(':checkbox').each(function() {
+                    $(this).prop('checked', false);
+                  });
+                }
+            });
+              
+            // Uncheck "Select All" if any other checkbox is unchecked  
+            $('.check').click(function(event) {
+                $('.check').each(function() {
+                    if (this.checked == false){
+                       $('#select-all').prop('checked', false);
+                       
+                    }
+                });
+            });
+            
+            // Check "Select All" if all other checkboxes are checked
+            $('.check').change(function() {
+                allchecked = true;
+                $('.check').each(function() {
+                    if (this.checked == false) {
+                        allchecked = false;
+                    }
+                });
+                if (allchecked == true) {
+                    $('#select-all').prop('checked', true);
+                }
+                allchecked = "";
+            });
+        });
+    </script>
+    
+    <?php
+        // Array of children to sign in/out
+        $names = $_POST['Name'];
+        $signing = "";
+        
+        // If sign in gets clicked
+        if(isset($_POST['signinbutton']))
+        {
+            // Validate at least one checkbox was clicked
+            if(!empty($names)) {
+                $signing = 0;
+                echo '<script type="text/javascript">',
+                'signInForm();',
+                '</script>'; 
+            }
+            else {
+                echo '<script type="text/javascript">',
+                'showSelectChild();',
+                '</script>'; 
+            }
+        }
+        
+        // If sign out gets clicked
+        if(isset($_POST['signoutbutton']))
+        {
+            // Validate at least one checkbox was clicked
+            if(!empty($names)) {
+                $signing = 1;
+                echo '<script type="text/javascript">',
+                'signOutForm();',
+                '</script>'; 
+            }
+            else {
+                echo '<script type="text/javascript">',
+                'showSelectChild();',
+                '</script>'; 
+            }
+        }
+        
+        // Store values in database
+        if(isset($_POST['submit']))
+        {
+            $date = $_POST['date'];
+            $time = $_POST['time'];
+            $esign = $_POST['e-sign'];
+            
+            // Signing in 
+            if($signing == 0){
+                // Debugging
+                echo '<script type="text/javascript">',
+                'console.log("Signing in");',
+                '</script>'; 
+                if (!empty($names)){
+                    echo '<script type="text/javascript">',
+                            'console.log("Array is not empty. Passed if statement.);',
+                            '</script>'; 
+                    foreach($names as $childID) { // Work in progress
+                        echo '<script type="text/javascript">',
+                            'console.log("Entered foreach loop");',
+                            '</script>'; 
+                        $SignInQuery = "INSERT INTO Log (Log_ID, Child_ID, Log_Date, Sign_In_Time, E_Sign_In) 
+                        VALUES (NULL, $childID, $date, $time, $esign)";
+                
+                        if ($conn->query($SignInQuery) === TRUE) {
+                            echo '<script type="text/javascript">',
+                            'console.log("New record created");',
+                            '</script>'; 
+                        } else {
+                            echo '<script type="text/javascript">',
+                            'console.log(',"Error: " . $sql . "<br>" . $conn->error,');',
+                            '</script>'; 
+                        }
+                    } 
+                }
+                else{
+                    // Debugging
+                    echo '<script type="text/javascript">',
+                            'console.log("Empty.");',
+                            '</script>'; 
+                }
+            }
+            // Signing out
+            else if ($signing == 1){
+                // Debugging
+                echo '<script type="text/javascript">',
+                'console.log("signing out");',
+                '</script>'; 
+            }
+            
+            // Debugging
+            echo '<script type="text/javascript">',
+                'console.log("Done");',
+                '</script>'; 
+        }
+    ?>
 </body>
 
 </html>
