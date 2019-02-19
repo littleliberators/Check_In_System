df9a21185220dfa8176cd0657e65a3b391461ee4
Emma Tsipan <s708959@sbuniv.edu>
2019-02-19 21:56:54 +0000

    Add Parent functionality

--- javascript/admin/Parent_Info.js
+++ javascript/admin/Parent_Info.js
@@ -1,7 +1,124 @@
 /* global $*/
-$(function() {
+/* global pin */
+/* global location */
+$('document').ready(function() {
     
+    // Click add if enter is pressed
+    $(document).keypress(function(e) {
+        if (e.key === "Enter") {
+            $("#add-button").click()
+        }
+    });
+
+    var pinNumber_state = false;
+
+    $('#add-button').on('click', function() {
+        // User input variables
+        var p1_fname = $('#p1-fn-input').val();
+        var p1_lname = $('#p1-ln-input').val();
+        var p2_fname = $('#p2-fn-input').val();
+        var p2_lname = $('#p2-ln-input').val();
+        var pin = $('#PIN').val();
+
+        if (p1_fname == "") {
+            $('#pin-message').show();
+            $('#pin-message').addClass("error");
+            $('#pin-message').text('Please enter a first name for Parent/Guarian 1');
+        }
+        else if (p1_lname == "") {
+            $('#pin-message').show();
+            $('#pin-message').addClass("error");
+            $('#pin-message').text('Please enter a last name for Parent/Guardian 1');
+        }
+        else if ((p2_fname == "") && !(p2_lname == "")) {
+            $('#pin-message').show();
+            $('#pin-message').addClass("error");
+            $('#pin-message').text('Please add a first name for Parent/Guardian 2');
+        }
+        else if (!(p2_fname == "") && (p2_lname == "")) {
+            $('#pin-message').show();
+            $('#pin-message').addClass("error");
+            $('#pin-message').text('Please add a last name for Parent/Guardian 2');
+        }
+        else if (validPIN() == true) {
+            // Check if pin exists
+            $.ajax({
+                url: '../../php/admin/Parent_Info.php',
+                type: 'post',
+                data: {
+                    'pinNumber_check': 1,
+                    'pinNum': pin,
+                },
+                success: function(response) {
+                    if (response == 'taken') {
+                        pinNumber_state = false;
+                        $('#pin-message').show();
+                        $('#pin-message').addClass("error");
+                        $('#pin-message').text('PIN is already taken');
+                    }
+                    else if (response == 'not_taken') {
+                        pinNumber_state = true;
+                        $('#pin-message').show();
+                        $('#pin-message').addClass("success");
+                        $('#pin-message').text('PIN is available');
+                    }
+
+                    // PIN is valid and is not taken
+                    if (pinNumber_state) {
+                        // proceed with form submission
+                        $.ajax({
+                            url: '../../php/admin/Parent_Info.php',
+                            type: 'post',
+                            data: {
+                                'save': 1,
+                                'p1_fname': p1_fname,
+                                'p1_lname': p1_lname,
+                                'p2_fname': p2_fname,
+                                'p2_lname': p2_lname,
+                                'pin': pin,
+                            },
+                            success: function(response) {
+                                $('#p1-fn-input').val('');
+                                $('#p1-ln-input').val('');
+                                $('#p2-fn-input').val('');
+                                $('#p2-ln-input').val('');
+                                $('#PIN').val('');
+                                closeForm();
+                                deleteTable();
+                                location.reload();
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
 });
+
+function validPIN() {
+    var pin = $('#PIN').val();
+    if (pin == '') {
+        $('#pin-message').show();
+        $('#pin-message').addClass("error");
+        $('#pin-message').text('Please enter a PIN');
+        return false;
+    }
+    else if (pin.length < 4) {
+        $('#pin-message').show();
+        $('#pin-message').addClass("error");
+        $('#pin-message').text('PIN needs to be at least 4 digits');
+        return false;
+    }
+    else if (!(/^\d+$/.test(pin))) {
+        $('#pin-message').show();
+        $('#pin-message').addClass("error");
+        $('#pin-message').text('PIN needs to be a number');
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
 function addParentForm() {
     // Show the log form
@@ -14,10 +131,8 @@ function addParentForm() {
 
 function closeForm() {
     // Close the form after close button is clicked 
-    $('#close-button').on('click', function() {
-        $('.add-parent-popup').hide();
-        $('.overlay').hide();
-    })
+    $('.add-parent-popup').hide();
+    $('.overlay').hide();
 }
 
 function deleteTable() {
--- php/admin/Parent_Info.php
+++ php/admin/Parent_Info.php
@@ -1,5 +1,6 @@
 <?php
-        
+    include('parent_process.php');
+    
     function populateParentTable() {
         include('../php_helpers/populateParentTable.php');
     }
@@ -48,7 +49,7 @@
             Please add parent first and last name(s) for one family.<br>
             <i>* Required fields</i>
         </div>
-        <form id="add-parent" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <form id="add-parent">
             <div class="names-container">
                 <div id="parent1-container">
                     <div class="parent-label">* Parent/Guardian 1</div>
@@ -74,79 +75,8 @@
                 <input class="input-box" id="PIN" name="PIN" type="text" pattern="\d{4,}" autocomplete="off" required
                 oninvalid="this.setCustomValidity('4 digit PIN required.')" oninput="this.setCustomValidity('')"/>
             </div>
-            <button id="add-button" name="add-parent">Add</button>
+            <div class="pin-message-label hide" id="pin-message">PIN number taken</div>
+            <button id="add-button" name="add-parent" type="button">Add</button>
         </form>
     </div>
-    
-    <?php
-         // If add gets clicked
-        if(isset($_POST['add-parent']))
-        {
-            $p1_first_name = $_POST['p1-first-name'];
-            $p1_last_name = $_POST['p1-last-name'];
-            $p2_first_name = $_POST['p2-first-name'];
-            $p2_last_name = $_POST['p2-last-name'];
-            $pinNum = $_POST['PIN'];
-            
-            // connect to the database
-            include('../connect-db.php');
-            
-            // Check if PIN already exists
-            // $checkQuery = "SELECT * from Family where PIN='$pinNum'";
-            // $result = mysqli_query($dbc, $checkQuery);
-            // $num_rows = $result->num_rows;
-            
-            // Iterate over the results that we got from the database
-            // if ($num_rows > 0){
-                
-            // }
-            
-            $query = "INSERT INTO Family (PIN) VALUES ('$pinNum')";
-            
-            // Creates a record in Family table
-            if ($dbc->query($query) === TRUE) {
-                $family_id = $dbc->insert_id;
-            } else {
-                echo "Error: " . $query . "<br>" . $dbc->error;
-            }
-            
-            $p1query = "INSERT INTO Parent (Family_ID, First_Name, Last_Name) VALUES ('$family_id', '$p1_first_name', '$p1_last_name')";
-            
-            // Creates a record in Parent table for Parent 1
-            if ($dbc->query($p1query) === FALSE) {
-                echo "Error: " . $p1query . "<br>" . $dbc->error;
-            }
-            
-            if (empty($p2_first_name)) {
-                if (empty($p2_last_name)){
-                    // Parent 2 is not added
-                }
-                else {
-                    /* Last name added, missing first name. Ask user to enter first name. */
-                }
-            }
-            else { // Add parent 2 to db
-                if (empty($p2_last_name))
-                {
-                    /* First name added, missing last name. Ask user to enter last name. */
-                }
-                else {
-                    $p2query = "INSERT INTO Parent (Family_ID, First_Name, Last_Name) VALUES ('$family_id', '$p2_first_name', '$p2_last_name')";
-                    
-                    // Creates a record in Parent table for Parent 1
-                    if ($dbc->query($p2query) === FALSE) {
-                        echo "Error: " . $p2query . "<br>" . $dbc->error;
-                    }
-                    
-                    echo '<script type="text/javascript">',
-                                'deleteTable();',
-                                '</script>'; 
-                    
-                     include('../php_helpers/populateParentTable.php');
-                }
-            }
-
-            $dbc->close();
-        }
-    ?>
 </body>
