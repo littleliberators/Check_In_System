f89901d5dbd8d727b5e993d93323a4e2ed27a786
Austin Newman <s708086@sbuniv.edu>
2019-03-10 01:40:23 +0000

    Wooh Works

--- php/Login.php
+++ php/Login.php
@@ -168,7 +168,7 @@ session_start();
     <img src="../images/Logo.png" align="Right" alt="Logo" />
   </div>
   <div id = "SBU">
-    <img stc="../images/SBU.png" align ="Left" alt="SBU" />
+    <img src="../images/SBU.png" align ="Left" alt="SBU" />
   </div>
   
 
