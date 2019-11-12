0d3aafc2979789ab6647eb469997cea851dc03c9
Ubuntu <ubuntu@ip-172-31-84-148.ec2.internal>
2019-11-12 23:00:27 +0000

    Report corrected

--- Controller/Admin/generateReport.php
+++ Controller/Admin/generateReport.php
@@ -228,13 +228,14 @@
                         $child_table .= add_new_week();
                     }
                 }
+                /* HIDES DAYS WITH NO SIGN IN, WE WANT THAT DATA SHOWN
                 if ($daysPresent == 0) {
                     $output .= '<div style="font-style: italic; font-size: 12pt; padding-top: none;">There are no recorded sign-in in or sign-out times for '.$name.'.</div>';
                 }
-                else {
+                else {*/
                     $output .= $child_table;
                     $output .= '</table><br />';
-                }
+               // }
             }
         }
         return $output;
