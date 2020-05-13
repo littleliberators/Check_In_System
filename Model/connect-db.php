04647884cc1bb10c0f42f304ff5932faddec380b
JGEURIN <cooper.geurin@gmail.com>
2020-05-13 01:51:34 +0000

    database credentials

--- Model/connect-db.php
+++ Model/connect-db.php
@@ -7,17 +7,28 @@
 ---------------------------------------------------------------------------*/
 
     // Database credentials
-    $host = "localhost";
-    $user = "admin";      //non root mysql user
-    $pass = "@BolivarR1";       //non root user password 
-    $db = "little_liberators";  //database name1
-    $port = 3306;
+    // $host = "localhost";
+    // $user = "admin";      //non root mysql user
+    // $pass = "@BolivarR1";       //non root user password 
+    // $db = "little_liberators";  //database name1
+    // $port = 3306;
+    
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    
+    $dbc = new mysqli($server, $username, $password, $db);
     
     // Connect to the database
-    $dbc = mysqli_connect($host, $user, $pass, $db, $port);
+    // $dbc = mysqli_connect($host, $user, $pass, $db, $port);
     
     // Check connection
     if ($dbc->connect_error) {
        die("Connection failed: " . $dbc->connect_error);
     } 
+    
+    
 ?>
