806aac7df2cc237dbd4dd26c0f4c3676c6da1ef1
JGEURIN <cooper.geurin@gmail.com>
2020-05-13 01:53:50 +0000

    database credentials fix

--- Model/connect-db.php
+++ Model/connect-db.php
@@ -7,28 +7,17 @@
 ---------------------------------------------------------------------------*/
 
     // Database credentials
-    // $host = "localhost";
-    // $user = "admin";      //non root mysql user
-    // $pass = "@BolivarR1";       //non root user password 
-    // $db = "little_liberators";  //database name1
-    // $port = 3306;
-    
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
-    
-    $dbc = new mysqli($server, $username, $password, $db);
+    $host = "localhost";
+    $user = "admin";      //non root mysql user
+    $pass = "@BolivarR1";       //non root user password 
+    $db = "little_liberators";  //database name1
+    $port = 3306;
     
     // Connect to the database
-    // $dbc = mysqli_connect($host, $user, $pass, $db, $port);
+    $dbc = mysqli_connect($host, $user, $pass, $db, $port);
     
     // Check connection
     if ($dbc->connect_error) {
        die("Connection failed: " . $dbc->connect_error);
     } 
-    
-    
 ?>
